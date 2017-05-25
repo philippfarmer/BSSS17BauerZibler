b7f0e9da65c81e6e628f642aacce27d0d87a3eb9
DavidZibler <strangedave11@gmail.com>
2017-05-25 15:55:34 +0200

    Put funtion ready

--- bs/functions.c
+++ bs/functions.c
@@ -11,14 +11,24 @@ int put(char *key, char *value, char *res)
     {
         if(strcmp(const char *)key_Value_Store.key[i], key)
         {
-            res = key_Value_Store.value[i];
+            res = &key_Value_Store.value[i];
             key_Value_Store.value[i] = value;
             return 0;
         }
     }
     if(current_length == STORELENGTH)
     {
-
+        printf("Nothing can be added\n");
+        return -1;
+    }
+    else
+    {
+        current_length++;
+        strcpy(key_Value_Store.key[current_length],key);
+        strcpy(key_Value_Store.value[current_length], value);
+        printf("Added Key: %s and Value: %s\n",key_Value_Store.key[i], key_Value_Store.value[i]);
+        res = &key_Value_Store.value[current_length];
+        return 0;
     }
 }
 
