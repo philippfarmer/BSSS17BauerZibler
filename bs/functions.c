151030ed052c0bf1ea37dfaf4cc166fad942411e
philippfarmer <philipp.bauer@smail.th-koeln.de>
2017-05-26 15:20:30 +0200

    several changes,

--- bs/functions.c
+++ bs/functions.c
@@ -5,24 +5,19 @@
 
 int current_length = 0;
 
-int put(char *key, char *value, char *res)
-{
-    for(int i = 0; i < STORELENGTH; i++)
-    {
-        if(strcmp(const char *)key_Value_Store.key[i], key)
-        {
+int put(char *key, char *value, char *res) {
+    for(int i = 0; i < STORELENGTH; i++) {
+        if(strcmp(const char *)key_Value_Store.key[i], key) {
             res = &key_Value_Store.value[i];
             key_Value_Store.value[i] = value;
             return 0;
         }
     }
-    if(current_length == STORELENGTH)
-    {
+    if(current_length == STORELENGTH) {
         printf("Nothing can be added\n");
         return -1;
     }
-    else
-    {
+    else {
         current_length++;
         strcpy(key_Value_Store.key[current_length],key);
         strcpy(key_Value_Store.value[current_length], value);
@@ -32,25 +27,19 @@ int put(char *key, char *value, char *res)
     }
 }
 
-int get(char *key, char *res)
-{
-    if(current_length == 0)
-    {
+int get(char *key, char *res) {
+    if(current_length == 0) {
         printf("No files saved!\n");
         return -1;
     }
-    else
-    {
-        for(i = 0; i < STORELENGTH; i++)
-        {
-            if(strcmp((const char *)key_Value_Store.key[i], key) == 0)
-            {
+    else {
+        for(i = 0; i < STORELENGTH; i++) {
+            if(strcmp((const char *)key_Value_Store.key[i], key) == 0) {
                 printf("Key: %s \n Value: %s",key_Value_Store.key[i], key_Value_Store.value[i]);
                 res = &key_Value_Store.value[i];
                 return 0;
             }
-            else
-            {
+            else {
                 printf("No Value found!\n");
                 return -1;
             }
@@ -58,35 +47,28 @@ int get(char *key, char *res)
     }
 }
 
-int del(char *key, char *res)
-{
-    if(current_length == 0)
-    {
-        for (int i = 0; i < STORELENGTH; i++)
-        {
+int del(char *key, char *res) {
+    if(current_length == 0) {
+        for (int i = 0; i < STORELENGTH; i++) {
             printf("No files saved\n");
             return -1;
         }
-        else
-        {
-            for(i = 0; i < STORELENGTH; i++)
-            {
+        else {
+            for(i = 0; i < STORELENGTH; i++) {
                 if(strcmp((const char *)key_Value_Store.key[i],key) == 0)
                 {
                     res = &key_Value_Store.value[i];
                     printf("Key: %s and Value: %s deleted.\n",key_Value_Store.key[i],key_Value_Store.value[i]);
                     strcpy(key_Value_Store.key[i], NULL);
                     strcpy(key_Value_Store.value[i], NULL);
-                    for(int j = current_length; b > i; b--)
-                    {
+                    for(int j = current_length; b > i; b--) {
                         key_Value_Store.key[b-1];
                         key_Value_Store.value[b-1];
                     }
                     current_length--;
                     return 0;
                 }
-                else
-                {
+                else {
                     printf("Nothing found!\n");
                     return -1;
                 }
--- bs/main.c
+++ bs/main.c
@@ -1,4 +1,60 @@
-#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 
 int main() {
+
+    int sock;                       // das Socket
+    struct sockaddr_in server;      // Socketadresse des Servers
+    struct sockaddr_in client;      // Socketadresse eines Clients
+    int fileDescriptor;             // Filedeskriptor für das Socket
+    int client_len;                 // Länge der ClientD-Daten
+    char in[2000];                  // Daten vom Client an den Server
+    char out[2000];                 // Daten vom Server an den Client
+    client_len = sizeof(client);    // Größe der Client-Nachricht
+
+    /* Anlegen eines Sockers */
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock < 0) {
+        perror("creating stream socket");
+        exit(1);
+    }
+
+    /* Binden einer Adresse an das Socket */
+    server.sin_familiy = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(4994);
+
+    bind(sock, (struct sockaddr *) &server, sizeof(server));
+
+    /* Auf Verbindung hören */
+    listen(sock, 5);
+
+    /* Verbindung akzeptieren */
+    fileDescriptor = accept(sock, &client, &client_len);
+
+    while (1) { // Server wartet auf Anfrage
+        fileDescriptor = accept(sock, &client, &client_len);
+        while (read(fileDescriptor, in, 2000) > 0) { // Daten vom Socket ==> in
+            // Hier wird nun mit den Daten des Clients gearbeitet.
+            if(strcmp(token[0], "PUT") == 0){
+                var = put(token[1], token[2], res);
+                puts("PUT funktioniert\n");
+            } else if (strcmp(token[0], "GET") == 0){
+                var = get(token[1], res);
+                puts("GET funktioniert\n");
+            } else if (strcmp (token[0], "DEL") == 0){
+                var = del(token[1], res);
+                puts("DEL funktioniert\n");
+            } else {
+                puts("Ungültige Eingabe vom Client\n");
+            }
+            write(fileDescriptor, out, strlen(out));// Daten vom Array out ==> Socket
+        }
+        close(fileDescriptor); // Der Client hat keine Daten mehr zu übertragen.
+    }
+
 }
